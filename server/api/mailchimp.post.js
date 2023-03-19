ab079c77120ec718855ccacd93d3ac99e2f39e72
valentin-morice <valentinmorice1@gmail.com>
2023-03-19 13:17:08 +0100

    allow cors

--- server/api/mailchimp.post.js
+++ server/api/mailchimp.post.js
@@ -13,10 +13,16 @@ export default defineEventHandler(async (event) => {
   const listId = "ac2862c58b";
   const subscribingUser = {
     email: body.email,
+    fName: body.fName,
+    lName: body.lName,
   };
 
   const response = await mailchimp.lists.addListMember(listId, {
     email_address: subscribingUser.email,
+    merge_fields: {
+      FNAME: subscribingUser.fName,
+      LNAME: subscribingUser.lName,
+    },
     status: "subscribed",
   });
 
