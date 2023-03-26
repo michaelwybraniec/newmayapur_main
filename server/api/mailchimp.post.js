d130d3c62316958f55e29cd98bbf1f31550f5555
valentin-morice <valentinmorice1@gmail.com>
2023-03-26 12:35:55 +0200

    add fields to mailchimp API call

--- server/api/mailchimp.post.js
+++ server/api/mailchimp.post.js
@@ -11,34 +11,64 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   const listId = "ac2862c58b";
+
+  let french;
+  let english;
+
+  let date = new Date(body.birthday).getDate();
+  let month = new Date(body.birthday).getMonth() + 1;
+
+  if (date < 10) {
+    date = "0" + date;
+  }
+
+  if (month < 10) {
+    month = "0" + month;
+  }
+
   const subscribingUser = {
     email: body.email,
     fName: body.fName,
     lName: body.lName,
     language: body.language,
+    birthday: `${month}/${date}`,
+    address: {
+      addr1: body.address.addr1,
+      city: body.address.city,
+      state: body.address.state,
+      zip: body.address.zip,
+      country: body.address.country,
+    },
   };
 
-  let french;
-  let english;
+  if (body.birthday === null) {
+    subscribingUser.birthday = "";
+  }
 
   if (subscribingUser.language === "20b75dca3e") {
     french = true;
-    english = false
+    english = false;
   } else if (subscribingUser.language === "c4e9098dd7") {
     french = false;
-    english = true
+    english = true;
   }
 
-  const response = await mailchimp.lists.addListMember(listId, {
+  await mailchimp.lists.addListMember(listId, {
     email_address: subscribingUser.email,
+    status: "subscribed",
     merge_fields: {
       FNAME: subscribingUser.fName,
       LNAME: subscribingUser.lName,
+      BIRTHDAY: subscribingUser.birthday,
+      ADRESS_1: subscribingUser.address.addr1,
+      CITY: subscribingUser.address.city,
+      STATEPROV: subscribingUser.address.state,
+      ZIP_CODE: subscribingUser.address.zip,
+      COUNTRY: subscribingUser.address.country,
     },
-    status: "subscribed",
     interests: {
       "20b75dca3e": french,
-      "c4e9098dd7": english,
+      c4e9098dd7: english,
     },
   });
 
