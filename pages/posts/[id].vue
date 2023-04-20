b6a0da236e43e426d4ed2364389c1e2b19663175
valentin-morice <valentinmorice1@gmail.com>
2023-04-20 18:26:24 +0200

    suggest more articles

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -17,6 +17,43 @@
       {{ post.data.attributes.Title }}
     </h1>
     <div v-html="markdown" class="mt-6 mb-16 markdown"></div>
+    <div class="lg:col-span-2 max-lg:order-2 space-y-4">
+      <h2 class="font-bold text-xl uppercase pb-2 border-b-2 border-base-300">
+        Read More
+      </h2>
+    </div>
+    <div v-if="posts.data.length > 0" v-for="post in [post1, post2]">
+      <div class="card sm:card-side bg-base-100 border-2 my-4 border-base-200">
+        <figure class="sm:w-[1200px]">
+          <img
+            :src="
+              config.public.strapiBase +
+              post.attributes.Thumbnail.data.attributes.formats.small.url
+            "
+            alt="Thumbnail"
+            class="h-[300px] max-md:w-full object-cover"
+          />
+        </figure>
+        <div class="card-body">
+          <div class="flex justify-between items-center">
+            <h2 class="card-title">{{ post.attributes.Title }}</h2>
+            <span
+              class="p-1 px-2 rounded-md text-white font-bold -translate-y-4 translate-x-4"
+              :class="setTagsBg(post.attributes.Category)"
+              >#{{ post.attributes.Category }}</span
+            >
+          </div>
+          <p>{{ post.attributes.Content.substr(0, 200) + "..." }}</p>
+          <div class="card-actions justify-end">
+            <NuxtLink
+              :to="'/posts/' + post.id"
+              class="btn btn-primary mt-4 btn-outline"
+              >Read More</NuxtLink
+            >
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,15 +61,41 @@
 import { marked } from "marked";
 
 const route = useRoute();
-const { findOne } = useStrapi();
+const { findOne, find } = useStrapi();
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 const post = await findOne("posts", route.params.id, {
   populate: ["Thumbnail"],
 });
 
+const posts = await find("posts", {
+  populate: ["Thumbnail"],
+  pagination: {
+    pageSize: 10,
+    page: 1,
+  },
+});
+
+const id = between(1, posts.data.length);
+
+const post1 = posts.data[id - 1];
+const post2 = posts.data[id];
+
 const markdown = computed(() => marked.parse(post.data.attributes.Content));
 
 const config = useRuntimeConfig();
+
+const setTagsBg = function (value) {
+  if (value === "Temple") {
+    return "bg-orange-600";
+  } else if (value === "Community") {
+    return "bg-blue-600";
+  } else {
+    return "bg-green-600";
+  }
+};
 </script>
 
 <style lang="postcss">
