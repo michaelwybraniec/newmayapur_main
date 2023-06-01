0a0c938166686d9b973622350b78624970ec6a4f
valentin-morice <valentinmorice1@gmail.com>
2023-06-02 01:19:09 +0200

    finish temple page

---
+++ components/misc/BottomTemplePage.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="pt-28 pb-36 2xl:px-16">
+    <div
+      class="grid grid-rows-1 lg:grid-cols-2 items-center px-4 2xl:px-20 gap-x-28"
+    >
+      <div class="order-2 lg:order-1 md:px-24 lg:px-0">
+        <div>
+          <div>
+            <div
+              class="p-5 px-1 pb-6 mx-auto inline border-2 border-base-300 bg-base-100 rounded-xl"
+            >
+              <Icon name="ion:logo-facebook" class="m-5" size="28px" />
+            </div>
+            <h2 class="text-xl uppercase mt-4 font-bold">Daily Darshan</h2>
+            <p class="pt-3 text-lg">
+              Get <b>Sri-Sri Gaura-Nitai</b>, <b>Sri-Sri Krishna-Balarama</b>,
+              <b>Sri Varahadeva</b>, and <b>Sri-Sri Radha-Govinda-Madhava</b>'s
+              <i>darshan</i> regularly, straight from your social media feed.
+            </p>
+            <NuxtLink
+              to="https://www.facebook.com/people/Daily-darsan-New-Mayapur-Iskcon-France/100064249227219/"
+              class="btn-primary btn mt-4"
+              >See on Facebook</NuxtLink
+            >
+          </div>
+          <div class="mt-12">
+            <div
+              class="p-5 px-1 pb-6 mx-auto inline border-2 border-base-300 bg-base-100 rounded-xl"
+            >
+              <Icon name="ion:logo-youtube" class="m-5" size="28px" />
+            </div>
+            <h2 class="text-xl uppercase mt-4 font-bold">Online Lectures</h2>
+            <p class="pt-3 text-lg">
+              Want to learn more about <b>Krishna Consciousness</b>? Our YouTube
+              channel has hundreds of recordings of lectures from holy
+              scriptures, like <i>Srimad-Bhagavatam</i> and
+              <i>Bhagavad-Gita</i>.
+            </p>
+            <NuxtLink
+              to="https://www.youtube.com/@NewMayapur"
+              class="bg-red-600 text-white border-0 hover:bg-red-700 btn mt-4"
+            >
+              Watch Now
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <div class="overflow-x-auto">
+        <h2 class="text-xl uppercase mb-5 ml-0.5 font-bold">Our Schedule</h2>
+        <div class="border-2 border-base-300 rounded-xl mb-20 lg:mb-0">
+          <table class="table table-zebra w-full">
+            <!-- head -->
+            <thead>
+              <tr>
+                <th></th>
+                <th>Time</th>
+                <th>Activity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- row 1 -->
+              <tr>
+                <th class="text-gray-300">1</th>
+                <td>04:30</td>
+                <td>Mangala Arati</td>
+              </tr>
+              <!-- row 2 -->
+              <tr>
+                <th class="text-gray-300">2</th>
+                <td>07:30</td>
+                <td>Darshan Arati</td>
+              </tr>
+              <!-- row 3 -->
+              <tr>
+                <th class="text-gray-300">3</th>
+                <td>08:00</td>
+                <td>Srimad-Bhagavatam Class</td>
+              </tr>
+              <tr>
+                <th class="text-gray-300">4</th>
+                <td>08:45</td>
+                <td>Breakfast</td>
+              </tr>
+              <tr>
+                <th class="text-gray-300">5</th>
+                <td>12:30</td>
+                <td>Midday Arati</td>
+              </tr>
+              <tr>
+                <th class="text-gray-300">6</th>
+                <td>13:30</td>
+                <td>Lunch</td>
+              </tr>
+              <tr>
+                <th class="text-gray-300">7</th>
+                <td>18:30</td>
+                <td>Gaura Arati</td>
+              </tr>
+              <tr>
+                <th class="text-gray-300">8</th>
+                <td>19:30</td>
+                <td>Diner</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
