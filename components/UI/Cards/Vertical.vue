4723eb1a501ab8b0a29f471023881e622380f5f4
valentin-morice <valentinmorice1@gmail.com>
2023-06-30 17:03:49 +0200

    finish farm page

---
+++ components/UI/Cards/Vertical.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-base-200 py-28 lg:px-16" id="volunteer">
+    <div
+      class="2xl:w-[1200px] shadow-lg flex flex-col-reverse xl:flex-row items-center gap-8 lg:gap-20 xl:mx-auto bg-base-100 rounded-2xl p-4 md:p-6 mx-4"
+    >
+      <div>
+        <h2 class="text-xl uppercase font-bold">{{ title }}</h2>
+        <p class="pt-3 text-lg">
+          {{ description }}
+        </p>
+        <a :href="link1">
+          <button class="btn btn-primary btn-outline mt-6">{{ btn1 }}</button>
+        </a>
+        <a :href="link2">
+          <button class="btn btn-primary btn-outline ml-2 mt-6">
+            {{ btn2 }}
+          </button>
+        </a>
+      </div>
+      <img class="md:max-w-lg rounded-xl" :src="img" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps([
+  "title",
+  "description",
+  "btn1",
+  "btn2",
+  "link1",
+  "link2",
+  "img",
+]);
+</script>
--- pages/farm.vue
+++ pages/farm.vue
@@ -1,215 +1,66 @@
 <template>
-  <div class="mx-auto py-28 2xl:px-16">
-    <div
-      class="grid grid-rows-1 2xl:grid-cols-2 px-4 2xl:px-20 items-center gap-x-4"
-    >
-      <div class="order-2 md:px-24 2xl:px-0">
-        <h2 class="text-xl uppercase font-bold">An Ecological Farm</h2>
-        <p class="pt-3 text-lg">
-          New Mayapur runs an ecological, non-violent farm on its domain. Its
-          goal is to provide viable solutions for tomorrow’s challenges, by
-          empowering individuals with theoretical and practical knowledge
-          pertaining to agriculture and animal husbandry, and by showing a
-          living exemple of what self-sufficiency could be, per Srila
-          Prabhupada's desire.
-        </p>
-      </div>
-      <img
-        class="order-1 max-w-xs md:max-w-lg mb-12 2xl:mb-0 mx-auto md:mb-24 2xl:mr-auto 2xl:ml-0 rounded-lg"
-        src="/img/bulls.jpeg"
-      />
-    </div>
-  </div>
-  <div class="bg-base-200">
-    <div class="pb-32 py-28 2xl:px-10">
-      <div
-        class="grid grid-cols-1 lg:grid-cols-3 px-4 lg:gap-x-10 2xl:px-20 items-start 2xl:gap-x-16"
-      >
-        <div class="order-2 lg:order-1 md:px-24 lg:px-0 mb-24 lg:mb-0">
-          <div class="p-5 pb-6 mx-auto inline bg-base-100 rounded-xl mb-16">
-            <Icon name="ion:leaf-outline" size="28px" />
-          </div>
-          <h2 class="text-xl mt-12 uppercase font-bold">Self-Sufficiency</h2>
-          <p class="pt-3 text-lg">
-            We have set three goals for our farm: the steady production of
-            grains and dairy, along with fruits and vegetables. Those are the
-            essential items needed for a community to provide for its own
-            maintenance, related to its food consumption.
-          </p>
-        </div>
-        <div class="order-2 lg:order-1 md:px-24 lg:px-0 mb-24 lg:mb-0">
-          <div class="p-5 pb-6 mx-auto inline bg-base-100 rounded-xl mb-16">
-            <Icon name="ion:heart-outline" size="28px" />
-          </div>
-          <h2 class="text-xl mt-12 uppercase font-bold">Cow Protection</h2>
-          <p class="pt-3 text-lg">
-            Cow protection is an essential part of <i>vaisnava</i> culture. It
-            is said that by caring for the cow, all benefits are bestowed upon
-            humanity. The cow is compared to a mother as it is maintaining our
-            existence, through its milk and other by-products.
-          </p>
-        </div>
-        <div class="order-2 lg:order-1 md:px-24 lg:px-0 mb-24 lg:mb-0">
-          <div class="p-5 pb-6 mx-auto inline bg-base-100 rounded-xl mb-16">
-            <Icon name="ion:book-outline" size="28px" />
-          </div>
-          <h2 class="text-xl mt-12 uppercase font-bold">
-            Educational Initiatives
-          </h2>
-          <p class="pt-3 text-lg">
-            Beyond a production-driven endeavor, the farm is primarily an
-            educational initiative. It is an experiment in living closer to
-            nature, in harmony with its cycles and inhabitants. As such, we will
-            provide workshops and guided tours in the future.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="pb-32 pt-28 lg:px-10 lg:w-[1000px] mx-auto">
-    <h2 class="text-2xl font-bold text-center mb-12">Our Farm's Activities</h2>
-    <div
-      class="flex flex-col mx-4 md:flex-row gap-4 items-center justify-center mb-16"
-    >
-      <img class="md:h-[245px] rounded-xl" src="/img/wheat.jpeg" />
-      <img class="md:h-[245px] rounded-xl" src="/img/quinoa.jpeg" />
-    </div>
-    <div class="mx-4">
-      <div
-        tabindex="0"
-        class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
-      >
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium">Ecology</div>
-        <div class="collapse-content">
-          <p>
-            Sustainability is currently the biggest challenge facing humanity,
-            and the practice of ecology is one of the core principle that will
-            enable us to create a healthy, long-lasting civilization. As such,
-            we in New Mayapur endeavour to permeate all of our activities with a
-            respect for nature and its gifts.
-          </p>
-        </div>
-      </div>
-      <div
-        tabindex="0"
-        class="collapse collapse-plus border mt-4 border-base-300 bg-base-100 rounded-box"
-      >
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium">Animal Husbandry</div>
-        <div class="collapse-content">
-          <p>
-            Our farm is home to two Jersey cows, who provide us with their milk
-            every day, along with their two calves. We also have a herd of four
-            billys who help us clean the forest by eating thorn bushes and other
-            unwanted vegetation. We take pride in the fact that none of the
-            animals we care for will ever see a slaughterhouse, and we therefore
-            shelter older bovids who might not be productive anymore, and who
-            would have seen their lives terminated in other structures.
-          </p>
-        </div>
-      </div>
-      <div
-        tabindex="0"
-        class="collapse collapse-plus mt-4 border border-base-300 bg-base-100 rounded-box"
-      >
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium">Grains Production</div>
-        <div class="collapse-content">
-          <p>
-            "All living bodies subsist on food grains, which are produced from
-            rain. Rains are produced by performance of <i>yajna</i> [sacrifice],
-            and <i>yajna</i> is born of prescribed duties" (Bhagavad-Gita, 3.14)
-          </p>
-          <p class="mt-2">
-            Grains are the staple of a balanced diet. If we are to reach full
-            self-sufficiency, we will have to provide grains daily to all the
-            members of our community. We currently have a few fields dedicated
-            to the production of wheat and amaranth, with which we are
-            manufacturing flour and baking bread throughout the year.
-          </p>
-        </div>
-      </div>
-      <div
-        tabindex="0"
-        class="collapse collapse-plus mt-4 border border-base-300 bg-base-100 rounded-box"
-      >
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium">
-          Fruits & Vegetables
-        </div>
-        <div class="collapse-content">
-          <p>
-            Next to the temple is situated the <i>Krishna-Balarama</i> garden,
-            which is producing various vegetables according to the season:
-            eggplants, tomatoes, spinach, potatoes, zucchinis, beetoroots,
-            carrots and more. We also have an apple orchard, which provides us
-            with delicious apple juice, and cherry trees all around the
-            property.
-          </p>
-        </div>
-      </div>
-      <div
-        tabindex="0"
-        class="collapse collapse-plus mt-4 border border-base-300 bg-base-100 rounded-box"
-      >
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium">Forest Management</div>
-        <div class="collapse-content">
-          <p>
-            New Mayapur has around 60 hectares of beautiful forest, where our
-            visitors can take beautiful walks and restore themselves. We are
-            actively using the wood for a variety of construction work, as well
-            as heating during the winter season, and make sure the forest stays
-            clean and healthy in order to have a reliable, long-term source of
-            good quality material.
-          </p>
-        </div>
-      </div>
-      <div
-        tabindex="0"
-        class="collapse collapse-plus border mt-4 border-base-300 bg-base-100 rounded-box"
-      >
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium">Flower Garden</div>
-        <div class="collapse-content">
-          <p>
-            French gardens are known all over the world for their aesthetics and
-            tidyness. New Mayapur is no exception, as we maintain a rose garden
-            situtated in front of the castle, that is a beautiful ornament to
-            the property and provides us with flowers for the worship of the
-            Deities for most of the year.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="bg-base-200 py-28 lg:px-16" id="volunteer">
-    <div
-      class="2xl:w-[1200px] shadow-lg flex flex-col-reverse xl:flex-row items-center gap-8 lg:gap-20 xl:mx-auto bg-base-100 rounded-2xl p-4 md:p-6 mx-4"
-    >
-      <div>
-        <h2 class="text-xl uppercase font-bold">Volunteering</h2>
-        <p class="pt-3 text-lg">
-          If you want to experience community life, pick up new skills, live
-          closer to nature, make new friends and create heart-warming memories,
-          why not try volunteering in New Mayapur for a couple weeks? We provide
-          accommodation and vegetarian meals, and have a wide variety of tasks
-          that need to be taken care of!
-        </p>
-        <a href="https://www.workaway.info/en/host/981887541472">
-          <button class="btn btn-primary btn-outline mt-6">Workaway</button>
-        </a>
-        <a href="https://wwoof.fr/en/host/9117-Bhakti-Yoga-eco-community">
-          <button class="btn btn-primary btn-outline ml-2 mt-6">Woofing</button>
-        </a>
-      </div>
-      <img class="md:max-w-lg rounded-xl" src="/img/volunteer.jpg" />
-    </div>
-  </div>
+  <UISectionImgText
+    :title="content.data.attributes.Header.Title"
+    :description="content.data.attributes.Header.Text"
+    :image="
+      config.public.strapiBase +
+      content.data.attributes.Header.Image.data.attributes.formats.medium.url
+    "
+    :bg-color="content.data.attributes.Header.BgColor"
+    :image-place="content.data.attributes.Header.ImagePlace"
+  />
+  <UISectionParagraphWrapperThree
+    :section0="content.data.attributes.Thirds.Section0"
+    :section1="content.data.attributes.Thirds.Section1"
+    :section2="content.data.attributes.Thirds.Section2"
+    :bg-color="content.data.attributes.Thirds.BgColor"
+  />
+  <UIAccordionWrapper
+    :title="content.data.attributes.AccordionSection.Title"
+    :images="content.data.attributes.AccordionSection.Images.data"
+  >
+    <UIAccordionItem
+      v-for="item in content.data.attributes.AccordionSection.AccordionItem"
+      :title="item.Title"
+      :text="item.Description"
+    />
+  </UIAccordionWrapper>
+  <UICardsVertical
+    :title="content.data.attributes.Volunteering.Title"
+    :description="content.data.attributes.Volunteering.Description"
+    :btn1="content.data.attributes.Volunteering.btn1"
+    :btn2="content.data.attributes.Volunteering.btn2"
+    :link1="content.data.attributes.Volunteering.link1"
+    :link2="content.data.attributes.Volunteering.link2"
+    :img="
+      config.public.strapiBase +
+      content.data.attributes.Volunteering.img.data.attributes.formats.medium
+        .url
+    "
+  />
 </template>
 
 <script setup>
+const { find } = useStrapi();
+
+const content = await find("farm-page", {
+  populate: {
+    Header: {
+      populate: "*",
+    },
+    Thirds: {
+      populate: "*",
+    },
+    AccordionSection: {
+      populate: "*",
+    },
+    Volunteering: {
+      populate: "*",
+    },
+  },
+});
+
+const config = useRuntimeConfig();
 useHead({
   title: "New Mayapur - Farm",
 });
