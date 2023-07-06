7a279ab79bba97a3eb40ff7595b777720ab27daf
valentin-morice <valentinmorice1@gmail.com>
2023-07-06 12:58:12 +0200

    add translate component to retreats

--- pages/retreats.vue
+++ pages/retreats.vue
@@ -1,29 +1,31 @@
 <template>
-  <UISectionImgText
-    :title="content.data.attributes.Header.Title"
-    :description="content.data.attributes.Header.Text"
-    :image="
-      config.public.strapiBase +
-      content.data.attributes.Header.Image.data.attributes.formats.small.url
-    "
-    :bg-color="content.data.attributes.Header.BgColor"
-    :image-place="content.data.attributes.Header.ImagePlace"
-  />
-  <UIAccordionWrapper
-    :title="content.data.attributes.AccordionSection.Title"
-    :images="content.data.attributes.AccordionSection.Images.data"
-  >
-    <UIAccordionItem
-      v-for="item in content.data.attributes.AccordionSection.AccordionItem"
-      :title="item.Title"
-      :text="item.Description"
+  <UtilitiesTranslateContent :renderComponent="renderComponent">
+    <UISectionImgText
+      :title="content.data.attributes.Header.Title"
+      :description="content.data.attributes.Header.Text"
+      :image="
+        config.public.strapiBase +
+        content.data.attributes.Header.Image.data.attributes.formats.small.url
+      "
+      :bg-color="content.data.attributes.Header.BgColor"
+      :image-place="content.data.attributes.Header.ImagePlace"
     />
-  </UIAccordionWrapper>
-  <UIMiscAccomodation
-    locale="en"
-    v-if="content.data.attributes.ShowAccomodation"
-  />
-  <UIMiscEventPreview locale="en" v-if="content.data.attributes.ShowEvents" />
+    <UIAccordionWrapper
+      :title="content.data.attributes.AccordionSection.Title"
+      :images="content.data.attributes.AccordionSection.Images.data"
+    >
+      <UIAccordionItem
+        v-for="item in content.data.attributes.AccordionSection.AccordionItem"
+        :title="item.Title"
+        :text="item.Description"
+      />
+    </UIAccordionWrapper>
+    <UIMiscAccomodation
+      locale="en"
+      v-if="content.data.attributes.ShowAccomodation"
+    />
+    <UIMiscEventPreview locale="en" v-if="content.data.attributes.ShowEvents" />
+  </UtilitiesTranslateContent>
 </template>
 
 <script setup>
